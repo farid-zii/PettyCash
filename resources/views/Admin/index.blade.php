8eeff10528f49baf4b6ef107b828273f90904283
Fanda Alfaridzi <fanda.alvaridzi01@gmail.com>
2023-03-15 21:36:51 +0700

    Update pertama

---
+++ resources/views/Admin/index.blade.php
@@ -0,0 +1,5 @@
+@extends('admin.layouts.main')
+
+@section('isi')
+    <h1>HALAMAN DIREKTUR</h1>
+@endsection
---
+++ resources/views/Direktur/index.blade.php
@@ -0,0 +1,5 @@
+@extends('direktur.layouts.main')
+
+@section('isi')
+    <h1>HALAMAN DIREKTUR</h1>
+@endsection
